5bfa40af26f8f2030f19fb3284e85fc765703499
IggyDrewAWS-at-647286605986 <akton29@yahoo.it>
2021-12-03 19:35:55 +1100

    Day 3

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,56 +1,101 @@
-var x = 0
-var y = 0
-var aim = 0
+import kotlin.math.roundToInt
 
 fun main() {
-    fun runCommand(command: String) {
-        val commandParams = command.split(" ")
-        val commandFunction = commandParams[0]
-        val amount = commandParams[1].toInt()
+    val exampleInput = listOf(
+        "00100", "11110", "10110", "10111", "10101", "01111",
+        "00111", "11100", "10000", "11001", "00010", "01010")
 
-        when (commandFunction) {
-            "down" -> y += amount
-            "up" -> y -= amount
-            "forward" -> x += amount
-        }
+    val input = readInput("Day03_test")
+
+    println(part1(exampleInput))
+    println(part1(input))
+
+    println(part2(exampleInput))
+    println(part2(input))
+}
+
+fun part1(input: List<String>): Int {
+    // binary string length of 12
+    val binaryOnes = mutableListOf<Int>()
+    for (i in input[0].indices) {
+        binaryOnes.add(0)
     }
+    var numberOfLines = 0
 
-    fun runCommandWithAim(command: String) {
-        val commandParams = command.split(" ")
-        val commandFunction = commandParams[0]
-        val amount = commandParams[1].toInt()
-
-        when (commandFunction) {
-            "down" -> aim += amount
-            "up" -> aim -= amount
-            "forward" -> {
-                x += amount
-                y += amount * aim
+    input.forEach {
+        numberOfLines++
+        for (i in it.indices) {
+            if (it[i] == '1') {
+                binaryOnes[i]++
             }
         }
     }
 
-    fun part1(input: List<String>): Int {
-        input.forEach {
-            runCommand(it)
-        }
+    var binaryGamma = ""
+    var binaryEpsilon = ""
+    for (i in binaryOnes.indices) {
+        val currentGamma = if (binaryOnes[i] > numberOfLines / 2) { "1" } else { "0" }
+        val currentEpsilon = if (binaryOnes[i] > numberOfLines / 2) { "0" } else { "1" }
 
-        return x * y
+        binaryGamma += currentGamma
+        binaryEpsilon += currentEpsilon
     }
 
-    fun part2(input: List<String>): Int {
-        input.forEach {
-            runCommandWithAim(it)
+    val gamma = binaryGamma.toInt(2)
+    val epsilon = binaryEpsilon.toInt(2)
+
+    return gamma * epsilon
+}
+
+fun part2(input: List<String>): Int {
+    val oxygen = createBinaryString(input, true)
+
+    val co2 = createBinaryString(input, false)
+
+    val co2Value = co2[0].toInt(2)
+    val oxygenValue = oxygen[0].toInt(2)
+
+    return co2Value * oxygenValue
+}
+
+fun onlyOnes(it: String, currentCharacter: Int) = it[currentCharacter] == '1'
+
+fun onlyZeros(it: String, currentCharacter: Int) = it[currentCharacter] == '0'
+
+fun createBinaryString(list: List<String>, isOxygen: Boolean): List<String> {
+    var variableList = list.toList()
+    for (currentCharacter in variableList.indices) {
+        if (variableList.size == 1) {
+            break
+        }
+        var onesCounter = 0
+        variableList.forEach {
+            if (it[currentCharacter] == '1') {
+                onesCounter++
+            }
         }
 
-        return x * y
+        variableList = filterList(isOxygen, onesCounter, variableList, currentCharacter)
+
     }
+    return variableList
+}
 
-
-    val input = readInput("Day02_test")
-    println(part1(input))
-
-    x = 0
-    y = 0
-    println(part2(input))
+private fun filterList(
+    isOxygen: Boolean,
+    onesCounter: Int,
+    variableList: List<String>,
+    currentCharacter: Int
+) = if (isOxygen) {
+    if (onesCounter >= (variableList.size / 2.0).roundToInt()) {
+        variableList.filter { onlyOnes(it, currentCharacter) }
+    } else {
+        variableList.filter { onlyZeros(it, currentCharacter) }
+    }
+} else {
+    if (onesCounter >= (variableList.size / 2.0).roundToInt()) {
+        variableList.filter { onlyZeros(it, currentCharacter) }
+    } else {
+        variableList.filter { onlyOnes(it, currentCharacter) }
+    }
 }
